ad74b7880ea0085d7d9b24e3907cbe7f29043299
Darío Cavuotti <des@riseup.net>
2017-04-25 09:50:08 -0300

    Tidy up load strategies

--- gen.zsh
+++ gen.zsh
@@ -1,7 +1,8 @@
+# vim: set sw=2 ts=2 et
 # Clones a plugin locally
 gen\clone () {
   typeset -A plugin; plugin=($@)
-  if [[ -d ${plugin[path]} ]]; then    
+  if [[ -d ${plugin[path]} ]]; then
     return 0
   fi
   git clone -- "${plugin[url]}" "${plugin[path]}"
@@ -11,43 +12,41 @@ gen\clone () {
 gen\load () {
   typeset -A plugin; plugin=($@)
 
-  # should be configurable, ie: form theme context should only look for .zsh-theme,
-  # or loc, for plugin context should only look for .plugin.zsh or loc, if given
-  # a loc it should fail when no $loc/$loc.plugin.zsh/$loc.zsh-theme/$loc.zs etc
-  # do no exist
   typeset -a strategies=(dot-plugin location init) # zsh-theme zsh sh
   typeset -a list;
   gen\load\list $strategies
 
+  if ! gen\load\strategy\source $list; then
+    return 1
+  fi
+
+  return 0
+}
+
+gen\load\strategy\source () {
+  local list=$1
   for line in $list; do
     source "$line"
     return 0
   done
-  
+
   return 1
 }
+
 
 # Load list
 gen\load\list () {
   gen\load\strategy\location () {
     typeset -A plugin; plugin=($@)
-    if [[ ${plugin[loc]} == '/' ]]; then
+    if [[ ${plugin[loc]} == "" || ${plugin[loc]} == "/" ]]; then
       return
     fi
 
-    local files=("${plugin[path]}/${plugin[loc]}.plugin.zsh"
-      "${plugin[path]}/${plugin[loc]}.zsh-theme"
-      "${plugin[path]}/${plugin[loc]}.zsh"
-      "${plugin[path]}/${plugin[loc]}"
+    list=(${plugin[path]}/${plugin[loc]}.plugin.zsh(N)
+      ${plugin[path]}/${plugin[loc]}.zsh-theme(N)
+      ${plugin[path]}/${plugin[loc]}.zsh(N)
+      ${plugin[path]}/${plugin[loc]}(N)
     )
-
-    local file
-    for file in $files; do
-      if [[ -f $file ]]; then
-        list+=$file
-        break;
-      fi
-    done
   }
 
   gen\load\strategy\dot-plugin () {
@@ -57,10 +56,7 @@ gen\load\list () {
 
   gen\load\strategy\init () {
     typeset -A plugin; plugin=($@)
-    local file="${plugin[path]}/init.zsh"
-    if [[ -f $file ]]; then
-      list+=$file
-    fi
+    list=(${plugin[path]}/init.zsh(N))
   }
 
   gen\load\strategy\zsh-theme () {
@@ -85,6 +81,9 @@ gen\load\list () {
     fi
   done
 }
+
+
+
 
 # Parse cli arguments [user/plugin] [location]
 gen\parse () {
